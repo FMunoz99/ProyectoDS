6dcb57eb6fcd1ae1f429eaa54113a795c2487ed3
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-11-16 13:03:31 -0500

    hola

--- backend/src/main/java/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/config/SecurityConfig.java
@@ -39,11 +39,8 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers("/noticias/**").permitAll()  // Permitir acceso público a /noticias
-                                .requestMatchers("/estudiante/**", "/empleado/**", "/incidente/**", "/objetoPerdido/**")  // Rutas protegidas que requieren autenticación
-                                .authenticated()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")  // Solo admin puede acceder a rutas /admin
-                                .anyRequest().authenticated()  // Para cualquier otra ruta se requiere autenticación
+                                .requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .build();
     }
