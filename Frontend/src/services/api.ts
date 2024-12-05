ee3a5cb4e7428f50f235e02632799b6506ec08a3
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-12-04 21:18:34 -0500

    a

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Aquí se configura la URL base de tu API (ajusta según tu configuración)
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Asegúrate de que esta URL sea la correcta
+  baseURL: 'http://localhost:8080', // Asegúrate de que esta URL sea la correcta
   headers: {
     'Content-Type': 'application/json',
   },
